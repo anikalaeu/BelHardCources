f9d06c5665b2ff406e551e6b6b5ad27a8c982e4d
MaxShumikhin <akitomc@gmail.com>
2020-01-27 21:04:19 +0300

    init

---
+++ src/by/belhard/j20/AliakseiNikalaeu/Lessons/Lesson01/FirstMain.java
@@ -0,0 +1,20 @@
+package by.belhard.j20.AliakseiNikalaeu.Lessons.Lesson01; //объявление пакета
+
+public class FirstMain {   //объявление класса
+
+    public static void main(String[] args) { //объявление метода
+
+
+
+
+        var i = 6 * 7;
+        System.out.println("Hello World!!!");
+        System.out.println("The answer is " + i + "!");
+
+
+    }
+
+
+    }
+
+}
